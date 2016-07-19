09277b1f8ba24881596a3645c19aaca55c943575
Richard <richardzy.chen@mail.utoronto.ca>
2016-07-19 09:50:22 -0400

    first commit

---
+++ public/assets/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    setTimeout(function() {
+        $(".logo").transition({opacity: 1}, 4000);
+    }, 1000);
+    setTimeout(function() {
+        $(".button").transition({opacity: 1}, 4000);
+    }, 3000);
+})
